7953c1f9bbd56f46fb1befc412c7b674ed806a68
Audrey <alee2@olin.edu>
2020-12-10 13:11:42 -0500

    Put back branch and jump in move.asm Need to get reg_dv to cpu reg file to be able to access it and compare it when beq

--- asm/move.asm
+++ asm/move.asm
@@ -7,9 +7,9 @@ mosi:
 miso:
   mfc0 $t2, $t0 # copies register zero from the coprocessor to register t1
               # look at mfc0 in the hex file
-  # beq  $t1, $t0, finish
+  beq  $t1, 0x1, finish #compare to dv_reg
   # # addi $t1, $zero, 0x16
-  # j miso
+  j miso
 
 # mfc0 $t1, 2
 
