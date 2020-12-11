0e9c686fb53e547f46b804472256fbe434b1787d
itschrislee <clee4@olin.edu>
2020-12-11 01:24:37 +0000

    no changes

--- asm/move.asm
+++ asm/move.asm
@@ -7,7 +7,7 @@ mosi:
 miso:
   mfc0 $t2, $t0 # copies register zero from the coprocessor to register t1
               # look at mfc0 in the hex file
-  beq  $t1, 0x1, finish #compare to dv_reg
+  beq  $t1, $t2, finish #compare to dv_reg
   # # addi $t1, $zero, 0x16
   j miso
 
