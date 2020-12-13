a9c138d13f5b4c35b5518408339a62f34dd7fa69
itschrislee <clee4@olin.edu>
2020-12-13 01:09:44 +0000

    Mosi bug where control reg is always 1

--- asm/move.asm
+++ asm/move.asm
@@ -4,30 +4,30 @@ addi $t1, $zero, 0x16
 # MOSI TEST
 # MOSI: $t2
 # MOSI DV: $t3
-# mosi:
-# mtc0 $t1, $t2
-# mfc0 $t3, $t3
-# bne  $t3, $zero, finish
-# j mosi
+mosi:
+mtc0 $t1, $t2
+mfc0 $t3, $t3
+bne  $t3, $zero, finish
+j mosi
 
 
 
 # MISO TEST
 # MISO:    $t0
 # MISO_DV: $t1
-miso1:
-  mfc0 $t2, $t0
-  mfc0 $t3, $t1
-  bne  $t3, $zero, miso2
-  # # addi $t1, $zero, 0x16
-  j miso1
-
-miso2:
-  mfc0 $t4, $t0
-  mfc0 $t3, $t1
-  bne  $t3, $zero, finish
-  # # addi $t1, $zero, 0x16
-  j miso2
+# miso1:
+#   mfc0 $t2, $t0
+#   mfc0 $t3, $t1
+#   bne  $t3, $zero, miso2
+#   # # addi $t1, $zero, 0x16
+#   j miso1
+#
+# miso2:
+#   mfc0 $t4, $t0
+#   mfc0 $t3, $t1
+#   bne  $t3, $zero, finish
+#   # # addi $t1, $zero, 0x16
+#   j miso2
 
 
 
