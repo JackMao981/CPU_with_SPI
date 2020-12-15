36805e0d35eac88b50b17596c24729d47f24a7da
itschrislee <clee4@olin.edu>
2020-12-15 05:32:02 +0000

    added comment to clarify how to use MISO and MOSI in assembly

--- asm/move.asm
+++ asm/move.asm
@@ -4,10 +4,14 @@ sll $t8, $t8, 16
 addu $t1, $t8, $t9
 # addi $t1, $zero, 0x0bba
 
-## MOSI TEST
-## MOSI: $t3
-## MOSI S: $t4
-## MOSI DV: $t5
+# MOSI TEST
+# REG MOSI: $t3
+# REG MOSI S: $t4
+# REG MOSI TR: $t5
+# How to send message
+# 1. Load data into REG MOSI
+# 2. Load a 1 into REG MOSI S to start sending message
+# 3. Check REG MOSI TR. When it is 1, data has finished sending
 start_mosi:
   mtc0 $t1, $t3
   li $t2, 1
@@ -22,9 +26,12 @@ mosi:
 ## MISO:    $t0
 ## MISO S:  $t1
 ## MISO_DV: $t2
+# How to receive message
+# 1. Load a 1 into REG MISO S to start sending message
+# 2. Keep storing REG MISO
+# 3. Check REG MISO DV. When it is 1, data has finished receiving
 start_miso:
   li $t2, 1
-  # mtc0 $t2, $t1
   mtc0 $t2, $t7
 miso:
   mfc0 $t4, $t0
